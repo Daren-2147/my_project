3574385ed99a6f5a2088039497dfdac2050721ef
Daren-2147 <dmanhd99@gmail.com>
2026-05-07 00:00:38 +0700

    Add files via upload

--- TDTT/test.py
+++ TDTT/test.py
@@ -1,10 +1,12 @@
-def selection_sort(arr):
-    n = len(arr)
-    for k in range(n):
-        min_idx = 0
-        for i in range(n):
-            if arr[min_idx]>arr[i]:
-                min_idx = i
-        arr[k] = arr[min_idx]
-    return arr
-print(selection_sort([1,5,4,2,7,6,5,9,10,11,15,24,26]))
+def check_perfect_num(n):
+    if n<2:
+        return False
+    tonguoc = 0
+    can = int(n**0.5)
+    i=2
+    while i<=can:
+        if n%i==0:
+            tonguoc = tonguoc +i +n//i
+    return tonguoc==n
+    
+print(check_perfect_num(6))
